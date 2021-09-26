9bde1eecd84a1bd80aafc882bdcbad41591fa8ee
Vasilii Laktiushin <lokky2789@gmail.com>
2021-09-26 23:55:16 +0300

    Project Wine Shop started

---
+++ src/main/java/com/example/demo/models/Wine.java
@@ -0,0 +1,30 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "wines")
+public class Wine
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "wine_id")
+    private Long wineId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "producer")
+    private String producer;
+
+    @Column(name = "price")
+    private Double price;
+
+}
